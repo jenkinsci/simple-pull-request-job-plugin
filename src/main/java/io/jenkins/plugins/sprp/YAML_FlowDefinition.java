873d5727f0a1a4128131e79d2eb516f986922382
gautamabhishek46 <gautam.abhishek46@gmail.com>
2018-05-21 20:10:57 +0530

    Git-Client-Plugin not working

--- src/main/java/io/jenkins/plugins/sprp/YAML_FlowDefinition.java
+++ src/main/java/io/jenkins/plugins/sprp/YAML_FlowDefinition.java
@@ -8,6 +8,11 @@ import hudson.scm.NullSCM;
 import hudson.scm.SCM;
 import jenkins.branch.Branch;
 //import jenkins.scm.api.;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.URIish;
+import org.jenkinsci.plugins.gitclient.FetchCommand;
+import org.jenkinsci.plugins.gitclient.Git;
+import org.jenkinsci.plugins.gitclient.GitClient;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowExecution;
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition;
@@ -23,6 +28,7 @@ import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class YAML_FlowDefinition extends FlowDefinition {
@@ -42,13 +48,19 @@ public class YAML_FlowDefinition extends FlowDefinition {
     @Override
     public FlowExecution create(FlowExecutionOwner owner, TaskListener listener,
                                           List<? extends Action> actions) throws Exception {
-        String script = "node {\n" +
-                        "    stage('Example') {\n" +
-                        "            echo 'Done Cloning'\n" +
-                        "    }\n" +
-                        "}";
+        String script = "";
 
         listener.getLogger().println(script);
+
+        GitClient git = Git.with(listener, ((WorkflowRun) owner.getExecutable()).getParent().getCharacteristicEnvVars())
+                .in(owner.getRootDir().getAbsoluteFile())
+                .getClient();
+        git.addRemoteUrl("origin", "https://github.com/gautamabhishek46/dummy.git");
+        FetchCommand gitFetch= git.fetch_();
+        ArrayList<RefSpec> refSpecs = new ArrayList<>();
+//        refSpecs.add(new RefSpec().setSource("https://github.com/gautamabhishek46/dummy.git"));
+        refSpecs.add(new RefSpec().setSourceDestination("master", "master"));
+        gitFetch.from(new URIish("https://github.com/gautamabhishek46/dummy.git"), null).execute();
         return new CpsFlowExecution(script, false, owner);
     }
 
