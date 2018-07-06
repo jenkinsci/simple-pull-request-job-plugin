260771c32c22430b82732ae400ddfad26dd26aee
gautamabhishek46 <gautam.abhishek46@gmail.com>
2018-07-06 23:26:42 +0530

    buildPrOnly option removed

--- src/main/java/io/jenkins/plugins/sprp/models/Configuration.java
+++ src/main/java/io/jenkins/plugins/sprp/models/Configuration.java
@@ -3,7 +3,6 @@ package io.jenkins.plugins.sprp.models;
 import java.util.ArrayList;
 
 public class Configuration {
-    private boolean buildPrOnly;
     private boolean pushPrOnSuccess;
     private ArrayList<String> prApprovers;
 
@@ -13,14 +12,6 @@ public class Configuration {
 
     public void setPrApprovers(ArrayList<String> prApprovers) {
         this.prApprovers = prApprovers;
-    }
-
-    public boolean isBuildPrOnly() {
-        return buildPrOnly;
-    }
-
-    public void setBuildPrOnly(boolean buildPrOnly) {
-        this.buildPrOnly = buildPrOnly;
     }
 
     public boolean isPushPrOnSuccess() {
